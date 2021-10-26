8952d5b6560fe4b84eb7a698dd2f98e4a3089d39
ErtugrulAlina <boykoalina03@gmail.com>
2021-10-26 12:52:07 +0300

    lesson13

--- src/state/Store.ts
+++ src/state/Store.ts
@@ -1,12 +1,9 @@
-
-import {tasksReducer} from "./todoList/tasks-reducer";
-import {todolistsReducer} from "./todoList/todolists-reducer";
-import {combineReducers, createStore} from "redux";
+import { tasksReducer } from './tasks-reducer';
+import { todolistsReducer } from './todolists-reducer';
+import { combineReducers, createStore } from 'redux';
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
-
-
 const rootReducer = combineReducers({
     tasks: tasksReducer,
     todolists: todolistsReducer
@@ -19,4 +16,3 @@ export type AppRootStateType = ReturnType<typeof rootReducer>
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
 window.store = store;
-
